eea7f6640f532050d3a9cac14875e82c04342d2c
Richard Hadden <richard.hadden@oeaw.ac.at>
2021-06-07 14:22:08 +0200

    making id requests across endpoints

--- ipif_client/ipif.py
+++ ipif_client/ipif.py
@@ -1,3 +1,7 @@
+import requests
+from typing import Callable
+
+
 class IPIFClientConfigurationError(Exception):
     pass
 
@@ -22,6 +26,20 @@ class IPIFSource(IPIFType):
     pass
 
 
+def _error_if_no_endpoints(func):
+    """Decorator for IPIF request methods to raise error
+    if no endpoints are set."""
+
+    def inner(self, *args, **kwargs):
+        if self._endpoints:
+            return func(self, *args, **kwargs)
+
+        else:
+            raise IPIFClientConfigurationError("No endpoints added to the IPIF client")
+
+    return inner
+
+
 class IPIF:
     def __init__(self, config={}):
         self._endpoints = {}
@@ -29,7 +47,7 @@ class IPIF:
         # Unpack endpoints from config into instance's endpoint dict
         if "endpoints" in config:
             for label, endpoint in config["endpoints"].items():
-                self.add_endpoint(label=label, uri=endpoint)
+                self.add_endpoint(name=label, uri=endpoint)
 
         # Create IPIF-type classes on the instance
         # each instance has its own classes, so class itself holds reference back
@@ -40,15 +58,15 @@ class IPIF:
         self.Factoid = type("Factoid", (IPIFFactoid,), {"_ipif_instance": self})
         self.Source = type("Source", (IPIFSource,), {"_ipif_instance": self})
 
-    def add_endpoint(self, label=None, uri=None):
-        if not label or not uri:
+    def add_endpoint(self, name=None, uri=None):
+        if not name or not uri:
             raise IPIFClientConfigurationError(
                 "An IPIF endpoint requires a label and endpoint uri"
             )
 
-        if label in self._endpoints:
+        if name in self._endpoints:
             raise IPIFClientConfigurationError(
-                f"An endpoint with label '{label}' has already been added. "
+                f"An endpoint with label '{name}' has already been added. "
                 "If this is a different endpoint, assign a unique label."
             )
 
@@ -57,4 +75,33 @@ class IPIF:
                 f"Endpoint '{uri}' has already been added."
             )
 
-        self._endpoints[label] = uri
+        self._endpoints[name] = uri
+
+    @_error_if_no_endpoints
+    def _request_single_object_by_id(self, endpoint_name, ipif_type, id_string):
+
+        URL = f"{self._endpoints[endpoint_name]}{ipif_type.lower()}/{id_string}"
+        print(URL)
+        try:
+            resp = requests.get(URL)
+        except requests.exceptions.ConnectionError:
+            return {"IPIF_STATUS": "Request failed"}
+
+        if resp.status_code == 200:
+            return resp.json()
+        elif resp.status_code == 404:
+            return None
+        else:
+            return {"IPIF_STATUS": "Request failed"}
+
+    @_error_if_no_endpoints
+    def _request_id_from_endpoints(self, ipif_type, id_string):
+        results = {}
+        for endpoint_name, endpoint_uri in self._endpoints.items():
+            result = self._request_single_object_by_id(
+                endpoint_name, ipif_type, id_string
+            )
+            if result:
+                results[endpoint_name] = result
+
+        return results
--- tests/test_ipif_client.py
+++ tests/test_ipif_client.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 
 from ipif_client import __version__
 
@@ -24,7 +25,7 @@ def test_add_endpoint():
 
     # Missing one or other arg also raises exception
     with pytest.raises(IPIFClientConfigurationError):
-        ipif.add_endpoint(label="something")
+        ipif.add_endpoint(name="something")
     with pytest.raises(IPIFClientConfigurationError):
         ipif.add_endpoint(uri=URI)
 
@@ -37,11 +38,11 @@ def test_add_endpoint():
 
     # Test duplicate label causes exception
     with pytest.raises(IPIFClientConfigurationError):
-        ipif.add_endpoint(label="APIS", uri=URI)
+        ipif.add_endpoint(name="APIS", uri=URI)
 
     # Test duplicate uri causes exception
     with pytest.raises(IPIFClientConfigurationError):
-        ipif.add_endpoint(label="somethingElse", uri=URI)
+        ipif.add_endpoint(name="somethingElse", uri=URI)
 
 
 def test_ipif_init_with_endpoint_config():
@@ -68,9 +69,71 @@ def test_ipif_initialises_with_ipif_type_classes():
     assert ipif.Statement._ipif_instance is ipif
     assert ipif.Factoid._ipif_instance is ipif
 
-    ipif.add_endpoint(label="APIS", uri=URI)
+    ipif.add_endpoint(name="APIS", uri=URI)
 
     assert ipif.Person._ipif_instance._endpoints["APIS"] == URI
     assert ipif.Source._ipif_instance._endpoints["APIS"] == URI
     assert ipif.Statement._ipif_instance._endpoints["APIS"] == URI
     assert ipif.Factoid._ipif_instance._endpoints["APIS"] == URI
+
+
+def test_http_server_mock(httpserver):
+    httpserver.expect_request("/foobar").respond_with_json({"foo": "bar"})
+    # check that the request is served
+    assert requests.get(httpserver.url_for("/foobar")).json() == {"foo": "bar"}
+
+
+def test_error_if_no_endpoints_decorator():
+    ipif = IPIF()
+
+    with pytest.raises(IPIFClientConfigurationError):
+        ipif._request_single_object_by_id("APIS", "person", "the_id")
+
+    ipif.add_endpoint(name="APIS", uri=URI)
+
+
+def test_request_single_object_by_id(httpserver):
+
+    httpserver.expect_request("/persons/anIdString").respond_with_json(
+        {"@id": "anIdString"}
+    )
+
+    ipif = IPIF()
+    ipif.add_endpoint("TEST", uri=httpserver.url_for("/"))
+
+    # Test we get a result as planned
+    assert ipif._request_single_object_by_id("TEST", "Persons", "anIdString") == {
+        "@id": "anIdString"
+    }
+
+    httpserver.expect_request("/persons/MISSING").respond_with_data(
+        "Not found", status=404
+    )
+
+    # Test we get a None if the request 404's
+    assert ipif._request_single_object_by_id("TEST", "Persons", "MISSING") == None
+
+    # Otherwise, set a result status of Failed
+    assert ipif._request_single_object_by_id("TEST", "Persons", "ARSE") == {
+        "IPIF_STATUS": "Request failed"
+    }
+
+
+def test_request_id_from_endpoints(httpserver):
+
+    httpserver.expect_request("/succeed/persons/anIdString").respond_with_json(
+        {"@id": "anIdString"}
+    )
+    httpserver.expect_request("/notFound/persons/anIdString").respond_with_data(
+        "Not found", status=404
+    )
+
+    ipif = IPIF()
+    ipif.add_endpoint("TEST_SUCCEED", uri=httpserver.url_for("/succeed/"))
+    ipif.add_endpoint("TEST_NOT_FOUND", uri=httpserver.url_for("/notFound/"))
+    ipif.add_endpoint("TEST_NOSERVER", uri="http://not_there.net")
+
+    result = ipif._request_id_from_endpoints("Persons", "anIdString")
+    assert result["TEST_SUCCEED"] == {"@id": "anIdString"}
+    assert result["TEST_NOSERVER"] == {"IPIF_STATUS": "Request failed"}
+    assert "TEST_NOT_FOUND" not in result
